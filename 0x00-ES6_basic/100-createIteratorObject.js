f0419a9985b0eda3509d9bca9aa464660376a8eb
BinyamMamo <binyammamo01@gmail.com>
2024-03-04 20:34:57 +0400

    edit 100-createIteratorObject.js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+  let employees = [];
+  for (const department in report.allEmployees) {
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+      employees = [...employees, ...report.allEmployees[department]];
+    }
+  }
+  return employees.values();
+}
