5f25361c7378d9b75a561b882caf02ef2becc1be
BinyamMamo <binyammamo01@gmail.com>
2024-03-04 20:55:16 +0400

    edit 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
+  // eslint-disable-next-line no-empty
   if (trueOrFalse) {
-    let taskInsideIf = true;
-    let task2InsideIf = false;
   }
 
   return [task, task2];
