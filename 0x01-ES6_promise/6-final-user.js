83d2cc109557b805c1bf9066f1bbe31350bbe5a6
BinyamMamo <binyammamo01@gmail.com>
2024-03-06 23:09:58 +0400

    edit 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,16 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  //
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise
+    .allSettled([user, photo])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }))).catch((err) => {
+      console.log(err);
+    });
 }
