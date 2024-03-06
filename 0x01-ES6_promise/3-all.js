e83349f6510e11556d3b9b38375be758a436525d
BinyamMamo <binyammamo01@gmail.com>
2024-03-06 20:52:40 +0400

    add 4-user-promise.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((result) => {
+  return new Promise.all([uploadPhoto(), createUser()]).then((result) => {
     console.log(result[0].body, result[1].firstName, result[1].lastName);
   }).catch(() => console.log('Signup system offline'));
 }
