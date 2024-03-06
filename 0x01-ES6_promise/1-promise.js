13de410b2bbb6764249341056ee2c09f014ee6ad
BinyamMamo <binyammamo01@gmail.com>
2024-03-06 17:07:00 +0400

    edit 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,10 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      const obj = {
+      resolve({
         status: 200,
         body: 'Success',
-      };
-      resolve(obj);
+      });
     } else {
       // eslint-disable-next-line prefer-promise-reject-errors
       reject('The fake API is not working currently');
