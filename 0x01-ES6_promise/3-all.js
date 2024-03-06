3d725a9ea4f855ba07c7db8a67dde661add807bf
BinyamMamo <binyammamo01@gmail.com>
2024-03-06 21:27:38 +0400

    edit 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,10 @@
-/* eslint-disable import/extensions */
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  return new Promise.all([uploadPhoto(), createUser()]).then((result) => {
-    console.log(result[0].body, result[1].firstName, result[1].lastName);
-  }).catch(() => console.log('Signup system offline'));
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
