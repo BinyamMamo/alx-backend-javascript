05936884485f0914e10683b695a32172d927c9e4
BinyamMamo <binyammamo01@gmail.com>
2024-03-06 20:30:47 +0400

    Refactor handleProfileSignup function to use async/await syntax.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  let photo;
-  let user;
-
-  uploadPhoto().then((obj) => {
-    photo = obj.body;
-    return createUser();
-  }).then((obj) => {
-    user = obj;
-  }).catch(() => console.log('Signup system offline'))
-    .finally(() => console.log(photo, user.firstName, user.lastName));
+export default async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(photo.body, user.firstName, user.lastName);
+  }
+  catch {
+    console.log('Signup system offline');
+  }
 }
