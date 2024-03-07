f74cd6c2c66f881cb45af68ae2e9799f2a7818f8
BinyamMamo <binyammamo01@gmail.com>
2024-03-07 23:16:45 +0400

    add

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  // Getter for sqft
+  get sqft() { return this._sqft; }
+
+  // Abstract method to be implemented by subclasses
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building')
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
