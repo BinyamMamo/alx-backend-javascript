f6a66d8b7a8f116f94eea0bc011fb5ce96d18aaf
BinyamMamo <binyammamo01@gmail.com>
2024-03-07 23:42:37 +0400

    Edit 7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,18 +1,14 @@
 export default class Airport {
   constructor(name, code) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-
+    if (typeof name !== 'string' || typeof code !== 'string') throw new Error();
     this._name = name;
     this._code = code;
   }
 
-  // returns the airport code
-  toString() {
-    return `[${this._code}]`;
-  }
+  get name() { return this._name; }
+  get code() { return this._code;}
+  get [Symbol.toStringTag]() { return this.code; }
+
+  set name(value) { this._name = value; }
+  set code(value) { this._code = value; }
 }
