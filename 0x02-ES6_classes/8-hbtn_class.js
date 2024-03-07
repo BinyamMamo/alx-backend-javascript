6200e3b0051d99f81b6a295d411560e255bdca41
BinyamMamo <binyammamo01@gmail.com>
2024-03-07 23:28:44 +0400

    add

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,20 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+      if (typeof size !== 'number')
+          throw new TypeError('Size must be a number');
+      if (typeof location !== 'string')
+          throw new TypeError('Location must be a string');
+      this._size = size;
+      this._location = location;
+  }
+
+  // returns the size
+  valueOf() {
+      return this._size;
+  }
+
+  // returns the location
+  toString() {
+      return this._location;
+  }
+}
