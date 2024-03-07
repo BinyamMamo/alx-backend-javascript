58e04d7cfb3f17f44ff09f277826221115b5f113
BinyamMamo <binyammamo01@gmail.com>
2024-03-07 22:38:22 +0400

    fix formatting issues in 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -11,26 +11,21 @@ export default class Currency {
 
   // getter for code
   get code() {
-    // eslint-disable-next-line no-underscore-dangle
     return this._code;
   }
-  
+
   // getter for name
   get name() {
-    // eslint-disable-next-line no-underscore-dangle
     return this._name;
   }
-  
-  // setters
+
   // setter for code
   set code(c) {
-    // eslint-disable-next-line no-underscore-dangle
     this._code = c;
   }
-  
+
   // setter for name
   set name(n) {
-    // eslint-disable-next-line no-underscore-dangle
     this._name = n;
   }
 }
