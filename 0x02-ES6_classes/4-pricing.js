5aae6ff2c33608730809cdff253c1ccce9980401
BinyamMamo <binyammamo01@gmail.com>
2024-03-07 23:03:45 +0400

    Refactor code to include type checking for the setters 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,38 +2,47 @@ import Currency from './3-currency'
 
 export default class Pricing {
   constructor(amount, currency) {
-      // store the attributes with a setter
-      this.amount = amount;
-      this.currency = currency;
+    // store the attributes with a setter
+    this.amount = amount;
+    this.currency = currency;
   }
 
   // Getter for amount
   get amount() {
-      return this._amount;
+    return this._amount;
   }
 
   // Getter for currency
   get currency() {
-      return this._currency;
+    return this._currency;
   }
 
   // Setter for amount
   set amount(a) {
-      this._amount = a;
+    if (typeof a !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = a;
   }
 
   // Setter for currency
   set currency(c) {
-      this._currency = c;
+    if (!(c instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = c;
   }
 
   // Method to display full price format
   displayFullPrice() {
-      return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   // Static method to convert price based on conversion rate
   static convertPrice(amount, conversionRate) {
-      return amount * conversionRate;
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
   }
 }
