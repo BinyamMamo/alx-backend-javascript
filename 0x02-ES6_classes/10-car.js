bf9bbd8f36a165c83a3dd6c0aed4a1b2bb585102
BinyamMamo <binyammamo01@gmail.com>
2024-03-07 23:45:05 +0400

    add 10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+export default class Car {
+  constructor(brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+  }
+
+  // clones the car
+  cloneCar() {
+      const symbol = Symbol('Car');
+      const clonedCar = Object.create(this);
+      clonedCar[symbol] = true;
+      return clonedCar;
+  }
+}
