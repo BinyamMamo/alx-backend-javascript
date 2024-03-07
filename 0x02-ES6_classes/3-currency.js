c66ed5a4c6de060b431469b931647eaa2954a6fb
BinyamMamo <binyammamo01@gmail.com>
2024-03-07 22:03:03 +0400

    add comments for getters and setter in 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -8,14 +8,19 @@ export default class Currency {
   displayFullCurrency() {
     return `${this.name} (${this.code})`;
   }
-  
-  /* eslint-disable no-underscore-dangle */
-  // getters
-  get code() { return this._code; }
-  get name() { return this._name; }
 
+  /* eslint-disable no-underscore-dangle */
+  // getter for code
+  get code() { return this._code; }
+  
+  // getter for name
+  get name() { return this._name; }
+  
   // setters
+  // setter for code
   set code(c) { this._code = c; }
+  
+  // setter for name
   set name(n) { this._name = n; }
   /* eslint-enable no-underscore-dangle */
 }
---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,8 @@
+import Currency from './3-currency'
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+}
