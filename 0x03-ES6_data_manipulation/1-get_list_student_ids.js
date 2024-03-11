c1379d6922e8c7dd197405f5b9e474baecd7f713
BinyamMamo <binyammamo01@gmail.com>
2024-03-11 19:41:18 +0400

    fix formatting issues with 1-get_list_student_ids.js add 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,3 @@
 export default function getListStudentIds(students) {
-	return Array.isArray(students) ? students.map((st) => st.id) : [];
+  return Array.isArray(students) ? students.map((st) => st.id) : [];
 }
