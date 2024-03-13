1bae4d77e722357950f962142db5c434d60a0f82
BinyamMamo <binyammamo01@gmail.com>
2024-03-13 13:37:48 +0400

    refactor 8-clean_set.js to return empty string if item is empty

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || typeof startString !== 'string') return '';
+  if (typeof startString !== 'string' || startString === '') return '';
   let result = '';
   for (const item of set) {
     if (item && item.startsWith(startString)) {
       result += `${item.slice(startString.length)}-`;
+    } else {
+      result += '';
     }
   }
   return result.slice(0, -1);
