5d9859c5f869f6e75e058741e19e32d04cf76284
BinyamMamo <binyammamo01@gmail.com>
2024-03-13 13:30:48 +0400

    refactor 8-clean_set.js to check if startString is really a string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (startString === '' || typeof startString !== 'string') return '';
   let result = '';
   for (const item of set) {
     if (item && item.startsWith(startString)) {
