780a0fbe01e707b2bd272a31e8c7c0ca5868e963
BinyamMamo <binyammamo01@gmail.com>
2024-05-06 12:16:54 +0400

    feat(3-read_file_async.js): refactor to use promises and callbacks

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,21 +1,34 @@
 // 3-read_file_async.js
 
-const fs = require('fs').promises;
+const fs = require("fs");
 
-async function countStudents(path) {
-    try {
-        const data = await fs.readFile(path, 'utf8');
-        const lines = data.split('\n').filter(line => line.trim() !== ''); // Remove empty lines
-        const students = lines.map(line => line.split(','));
-        const csStudents = students.filter(student => student[3] === 'CS');
-        const sweStudents = students.filter(student => student[3] === 'SWE');
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err)
+        reject(new Error("Cannot load the database"));
+      if (data)
+      {
+        const lines = data.split("\n").filter((line) => line.trim() !== "").slice(1);
+        const students = lines.map((line) => line.split(","));
+        const csStudents = students.filter((student) => student[3] === "CS");
+        const sweStudents = students.filter((student) => student[3] === "SWE");
 
         console.log(`Number of students: ${students.length}`);
-        console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map(s => s[0]).join(', ')}`);
-        console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map(s => s[0]).join(', ')}`);
-    } catch (error) {
-        throw new Error('Cannot load the database');
-    }
+        console.log(
+          `Number of students in CS: ${csStudents.length}. List: ${csStudents
+            .map((s) => s[0])
+            .join(", ")}`
+        );
+        console.log(
+          `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents
+            .map((s) => s[0])
+            .join(", ")}`
+        );
+        resolve(true);
+      }
+    });
+  });
 }
 
 module.exports = countStudents;
