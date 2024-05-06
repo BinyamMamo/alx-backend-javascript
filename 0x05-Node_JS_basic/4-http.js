bbf5a89ffd053249c7c2908301c3e6acc01b30ff
BinyamMamo <binyammamo01@gmail.com>
2024-05-06 14:55:15 +0400

    feat: create HTTP server instance

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,17 @@
+/**
+ * Creates an HTTP server that listens on the specified port and responds with the message "Hello Holberton School!".
+ *
+ * @module 4-http
+ * @exports app - The HTTP server instance.
+ */
+const http = require('http');
+
+const PORT = 1245;
+
+app = http.createServer((req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.listen(PORT);
+
+module.exports = app;
