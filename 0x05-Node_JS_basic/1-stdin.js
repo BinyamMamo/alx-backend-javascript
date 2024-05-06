0f38f7e6a930c116f77308f8ab1b37e4fe711f7e
BinyamMamo <binyammamo01@gmail.com>
2024-05-06 11:20:44 +0400

    feat(stdin): handle stdin data and exit events
    - Handle stdin data event to read user input
    - Handle process exit event to print closing message

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,14 +7,10 @@
  */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
+process.stdin.on('data', (INPUT) => {
+  process.stdout.write(`Your name is: ${INPUT.toString()}`);
 });
 
-process.stdin.on('end', () => {
+process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
