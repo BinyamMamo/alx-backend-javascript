8f739fd0a00074b23af8d5239244ff6dfecca78e
BinyamMamo <binyammamo01@gmail.com>
2024-05-06 12:33:07 +0400

    refactor(3-read_file_async.js): Improve code readability and consistency
    - Use single quotes for string literals
    - Split long lines for better readability
    - Use consistent spacing and formatting
    - Remove unnecessary comments

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,29 +1,30 @@
 // 3-read_file_async.js
 
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, "utf8", (err, data) => {
-      if (err)
-        reject(new Error("Cannot load the database"));
-      if (data)
-      {
-        const lines = data.split("\n").filter((line) => line.trim() !== "").slice(1);
-        const students = lines.map((line) => line.split(","));
-        const csStudents = students.filter((student) => student[3] === "CS");
-        const sweStudents = students.filter((student) => student[3] === "SWE");
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) reject(new Error('Cannot load the database'));
+      if (data) {
+        const lines = data
+          .split('\n')
+          .filter((line) => line.trim() !== '')
+          .slice(1);
+        const students = lines.map((line) => line.split(','));
+        const csStudents = students.filter((student) => student[3] === 'CS');
+        const sweStudents = students.filter((student) => student[3] === 'SWE');
 
         console.log(`Number of students: ${students.length}`);
         console.log(
           `Number of students in CS: ${csStudents.length}. List: ${csStudents
             .map((s) => s[0])
-            .join(", ")}`
+            .join(', ')}`,
         );
         console.log(
           `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents
             .map((s) => s[0])
-            .join(", ")}`
+            .join(', ')}`,
         );
         resolve(true);
       }
